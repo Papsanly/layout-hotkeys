0930211f1d3cdf2fc5b75e5952dc34683337228f
Max Zvyagintsev <max.post.space@gmail.com>
2024-02-14 15:10:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,14 @@ if [[ "${UID}" = "0" ]]; then
 fi
 
 DESTINATION=~/.local/share/gnome-shell/extensions/$(jq -r ".uuid" src/metadata.json)
+if [[ $? != 0 ]]; then
+    echo The extension installation path couldn\'t be generated!
+    exit 2
+fi
+
 if [[ -d $DESTINATION ]]; then
     echo The extension is already installed in path \"$DESTINATION\"!
-    exit 2
+    exit 3
 fi
 
 echo Copying files...
@@ -20,4 +25,4 @@ glib-compile-schemas $DESTINATION/schemas/
 
 unset DESTINATION
 
-echo Done!
+echo Done.
